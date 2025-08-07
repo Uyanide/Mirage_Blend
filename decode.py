a22fa2a5d36e159aab969fa9acd22d49390cfd60
Uyanide <pywang0608@foxmail.com>
2025-08-07 23:58:43 +0200

    没道理保守的

--- decode.py
+++ decode.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 from tqdm import tqdm
 import math
 
+
 def split(encodedImage: Image.Image, number: int, outputDir: Path):
     if encodedImage.mode != "RGBA":
         raise ValueError("输入图像必须是 RGBA 模式")
@@ -12,19 +13,19 @@ def split(encodedImage: Image.Image, number: int, outputDir: Path):
         raise ValueError("加载输入图像像素失败")
 
     width, height = encodedImage.size
-    scale = math.ceil(math.sqrt(math.ceil(number / 27)))
+    scale = math.ceil(math.sqrt(math.ceil(number / 32)))
     if width % scale != 0 or height % scale != 0:
         raise ValueError("输入图像格式错误")
     splitWidth = width // scale
     splitHeight = height // scale
 
     for i in tqdm(range(number), desc="解码图像"):
-        offset = i // 27
+        offset = i // 32
         offsetX = offset % scale
         offsetY = offset // scale
 
-        channel = (i % 27) // 7
-        bit = (i % 27) % 7
+        channel = (i % 32) // 8
+        bit = (i % 32) % 8
 
         image = Image.new("1", (splitWidth, splitHeight))
         pixels = image.load()
@@ -37,25 +38,14 @@ def split(encodedImage: Image.Image, number: int, outputDir: Path):
 
         image.save(outputDir / f"{i}.png", "PNG")
 
-    coverImage = Image.new("1", (width, height))
-    coverPixels = coverImage.load()
-    if not coverPixels:
-        raise ValueError("创建表图像失败")
-    for y in range(height):
-        for x in range(width):
-            v = (srcPixel[x, y][3] >> 6) & 1
-            coverPixels[x, y] = v
-
-    coverImage.save(outputDir / "cover.png", "PNG")
-    print(f"成功解码表图和 {number} 张图像到 {outputDir} 目录")
-
+    print(f"成功解码和 {number} 张图像到 {outputDir} 目录")
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description="从单张 PNG 图像中解码隐藏的图像")
     parser.add_argument("-i", "--input", type=Path, default=Path("encoded.png"), help="输入图像文件")
     parser.add_argument("-o", "--output", type=Path, default=Path("decoded"), help="输出图像目录")
-    parser.add_argument("-n", "--number", type=int, default=27, help="解码图像数量(不包含表图)")
+    parser.add_argument("-n", "--number", type=int, default=32, help="解码图像数量(不包含表图)")
     args = parser.parse_args()
 
     if args.number < 1:
--- encode.py
+++ encode.py
@@ -44,14 +44,11 @@ def binarize_image(image: Image.Image) -> Image.Image:
     return image.convert("1")
 
 
-def merge(imagePaths: list[Path], coverImage: Image.Image, size: tuple, scale: int) -> Image.Image:
+def merge(imagePaths: list[Path], size: tuple, scale: int) -> Image.Image:
     ret = Image.new("RGBA", size)
     retPixels = ret.load()
     if not retPixels:
         raise ValueError("加载像素失败")
-    coverPixels = coverImage.load()
-    if not coverPixels:
-        raise ValueError("加载表图像素失败")
 
     for i, path in tqdm(enumerate(imagePaths), total=len(imagePaths), desc="合并图像"):
         try:
@@ -60,13 +57,12 @@ def merge(imagePaths: list[Path], coverImage: Image.Image, size: tuple, scale: i
         except Exception as e:
             raise ValueError(f"加载图像 {path} 失败: {e}")
 
-        offset = i // 27
+        offset = i // 32
         offsetX = offset % scale
         offsetY = offset // scale
 
-        # skip MSB of each channel for coverImage
-        channel = (i % 27) // 7
-        bit = (i % 27) % 7
+        channel = (i % 32) // 8
+        bit = (i % 32) % 8
 
         imgPixels = image.load()
         if not imgPixels:
@@ -78,12 +74,6 @@ def merge(imagePaths: list[Path], coverImage: Image.Image, size: tuple, scale: i
                 current_pixel[channel] = current_pixel[channel] | ((1 if imgPixels[x, y] else 0) << bit)
                 retPixels[pos] = tuple(current_pixel)
 
-    for y in range(coverImage.size[1]):
-        for x in range(coverImage.size[0]):
-            v = (1 if coverPixels[x, y] else 0) << 7
-            r = retPixels[x, y]
-            retPixels[x, y] = (r[0] | v, r[1] | v, r[2] | v, r[3] | (1 << 6))
-
     return ret
 
 
@@ -93,27 +83,17 @@ if __name__ == "__main__":
     parser.add_argument("-y", "--height", type=int, default=800, help="输出图像高度")
     parser.add_argument("-i", "--input", type=Path, default=Path("images"), help="输入图像目录")
     parser.add_argument("-o", "--output", type=Path, default=Path("encoded.png"), help="输出图像路径")
-    parser.add_argument("-n", "--number", type=int, default=27, help="隐藏图像数量")
-    parser.add_argument("-c", "--cover", type=Path, help="表图(肉眼可分辨图像)路径")
+    parser.add_argument("-n", "--number", type=int, default=32, help="隐藏图像数量")
     args = parser.parse_args()
 
     if args.number < 1:
         raise ValueError("隐藏图像数量必须大于 0")
 
-    try:
-        coverImage = Image.open(args.cover) if args.cover else Image.new("1", (args.width, args.height))
-    except Exception as e:
-        raise ValueError(f"加载表图失败: {e}")
-    if not coverImage:
-        raise ValueError("加载表图失败")
-
-    imagePaths = load_images_from_dir(args.input, args.number)
+    imagePaths = sorted(load_images_from_dir(args.input, args.number))
     print(f"检测到 {len(imagePaths)} 张图像")
 
-    scale = math.ceil(math.sqrt(math.ceil(len(imagePaths) / 27)))
+    scale = math.ceil(math.sqrt(math.ceil(len(imagePaths) / 32)))
     splitSize = (args.width // scale, args.height // scale)
     mergeSize = (splitSize[0] * scale, splitSize[1] * scale)
 
-    coverImage = binarize_image(resize_cover(coverImage, mergeSize))
-
-    merge(imagePaths, coverImage, mergeSize, scale).save(args.output, "PNG")
+    merge(imagePaths, mergeSize, scale).save(args.output, "PNG")
